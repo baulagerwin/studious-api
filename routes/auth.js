292a6df4a96505825bde6d76f290e2bfc4801be7
Gerwin Baula <baulagerwin@gmail.com>
2023-01-10 20:04:20 +0800

    Login error message changed

--- routes/auth.js
+++ routes/auth.js
@@ -10,10 +10,10 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ username: req.body.username });
-  if (!user) return res.status(400).send("Invalid username.");
+  if (!user) return res.status(400).send("Invalid username or password.");
 
   let isPasswordValid = await bcrypt.compare(req.body.password, user.password);
-  if (!isPasswordValid) return res.status(400).send("Invalid password.");
+  if (!isPasswordValid) return res.status(400).send("Invalid username or password.");
 
   const token = user.generateAuthToken();
 
